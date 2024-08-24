a55fd907b1c776c58cfe9b83fa0d53a2cbad7ae2
MudiBro <103818451+MudiBro@users.noreply.github.com>
2024-08-24 19:15:53 +0300

    Update and rename allcombined.cpp to repeatforever.cpp

---
+++ repeatforever.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+string repeat;
+
+
+int main() {
+    cout << "What should be repeated?: "; cin >> repeat; cout << "\n";
+    while (true) {
+        cout << repeat << "\n";
+    }
+}
